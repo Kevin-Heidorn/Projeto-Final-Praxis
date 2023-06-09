b9e6b92e697380b05f1572cea8809d3b39b95cb9
Guilherme Almeida <gui.iwasaimingforthemoon@gmail.com>
2023-06-09 01:20:09 -0300

    Responsividade: index, cardapio, cadastro, cadastro-sucesso

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@ function dropdownMenu(){
     document.getElementById("dropdown__nav").classList.toggle("hide");
 
     
+}
+const whatsappBtn = document.getElementById('whatsapp__btn')
+const whatsappNum = document.getElementById('whatsapp__num')
+
+function togglePhoneNum(){
+    whatsappBtn.classList.toggle('hide')
+    whatsappNum.classList.toggle('hide')
+    
     console.log('sucesso')
 }
 
+const qrBtn = document.getElementById('qr-btn')
+const qr = document.getElementById('qr')
+
+function toggleQrCode(){
+    qrBtn.classList.toggle('hide')
+    qr.classList.toggle('hide')
+
+}
